e3f04db993aa9cc71b81dd68c9c9c09b36703942
NetDefender <danielsith@hotmail.com>
2022-10-13 17:06:55 +0200

    tslint

--- src/images.ts
+++ src/images.ts
@@ -1,3 +1,3 @@
-//const image2: string = require('./image.jpg')
+// const image2: string = require('./image.jpg')
 //import './styles.css';
 //const css: string = require('./styles.css')
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 console.log('hello')
+
+const t = 1;
