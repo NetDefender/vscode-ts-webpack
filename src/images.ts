801d27225fa141ba4ccc8dfe7b5e6d700b49e17c
NetDefender <danielsith@hotmail.com>
2022-10-15 19:52:50 +0200

    Image tricks

--- src/images.ts
+++ src/images.ts
@@ -1,3 +1,10 @@
+// Esto importa un string con la url de la imagen
 // const image2: string = require('./image.jpg')
+//import img from './image.jpg'
+
+// Esto hace que se cree un fichero css
 //import './styles.css';
-//const css: string = require('./styles.css')
+//const css = require('./styles.css')
+
+// Esto no genera un fichero css. Sino que almacena una variable solamente. Es necesario modificar el webpack.config
+// import cssModule from './styles.css'
