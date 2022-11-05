20f072578f818d87a1d5a7d690b23c8ea0c55533
NetDefender <danielsith@hotmail.com>
2022-11-05 03:20:07 +0100

    sidebar jedi mind tricks

--- src/sidebar-drag.ts
+++ src/sidebar-drag.ts
@@ -1,25 +1,25 @@
 import $ from 'jquery'
 
+let lastPosition = 200;
+$('#auto-open').on('click', () => showSideBar());
+
 let isCollapsed = false;
+
 const showSideBar = () => {
   isCollapsed = false;
-  $('#sidebar').css('right', '0');
+  $('#sidebar').css({'right': '0px', 'width': `${lastPosition}px`});
+  $('#user').trigger('focus');
+  $('#resizeSizeBar').show();
 }
 const hideSidebar = () => {
   isCollapsed = true;
+  moveSidebar(200, true);
   $('#sidebar').css('right', '-180px');
+  $('#resizeSizeBar').hide();
+  document.removeEventListener('mousemove', drag)
 }
 
-function moveSidebarBy (x: number) {
-  let newWidth = $('#sidebar').width()! + x;
-  $('#sidebar').css("width", `${newWidth}px`);
-}
-
-function moveSidebar (x: number) {
-  $('#sidebar').css("width", `${x}px`);
-}
-
-$('#manageSidebar').on('click', function() {
+$('#openCloseSidebar').on('click', function() {
   const right = $('#sidebar').css('right');
   if(right == '0px') {
     hideSidebar();
@@ -28,7 +28,7 @@ $('#manageSidebar').on('click', function() {
   }
 })
 
-$('#draggerLeft').on('mousedown', function(e) {
+$('#resizeSizeBar').on('mousedown', function(e) {
   if(!isCollapsed) {
     document.addEventListener('mousemove', drag);
     document.addEventListener('mouseup', () => document.removeEventListener('mousemove', drag));
@@ -39,8 +39,20 @@ function drag(e: MouseEvent) {
   let pos = window.innerWidth - e.clientX;
   if(pos < 200) {
     pos = 200;
-  } else if(pos > window.innerWidth - 100) {
+    hideSidebar();
+    return;
+  }
+
+  if(pos > window.innerWidth - 100) {
     pos = window.innerWidth - 100;
   }
   moveSidebar(pos)
 }
+
+function moveSidebar (x: number, ignoreLastPosition: boolean = false) {
+  if(!ignoreLastPosition){
+    lastPosition = x;
+  }
+  $('#sidebar').css("width", `${x}px`);
+}
+
