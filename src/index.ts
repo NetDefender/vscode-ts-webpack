edc814af05eeac52b5d02016ec61b9bef280df5b
NetDefender <danielsith@hotmail.com>
2022-12-01 19:48:32 +0100

    Title scroll

--- src/index.ts
+++ src/index.ts
@@ -30,19 +30,19 @@ function CreateTitleList() {
     }
     nodeList.push(newNode);
 
-    while(true){
+    while (true) {
       let prevNode = nodeStack.at(-1);
-      if(!prevNode) {
+      if (!prevNode) {
         break;
       }
-      if(prevNode.NodeLevel >= newNode.NodeLevel) {
+      if (prevNode.NodeLevel >= newNode.NodeLevel) {
         nodeStack.pop();
       } else {
         break;
       }
     }
     const prevNode = nodeStack.at(-1);
-    if(prevNode){
+    if (prevNode) {
       prevNode.Children.push(newNode);
     }
 
@@ -57,7 +57,7 @@ function RenderTitles(nodeId: string, nodeList: TitleNode[]) {
 
   const rootTitleNodes: TitleNode[] = [];
   nodeList.forEach(node => {
-    if(node.NodeLevel === 1) {
+    if (node.NodeLevel === 1) {
       rootTitleNodes.push(node);
     }
   });
@@ -68,9 +68,13 @@ function RenderTitles(nodeId: string, nodeList: TitleNode[]) {
     const parentNode = document.getElementById(parentNodeId)!;
     const newTitleNode = document.createElement('li');
     newTitleNode.id = titleNode.NodeId;
-    newTitleNode.innerHTML = `<a>${titleNode.NodeText}</a>`;
+    let fontLevel = titleNode.NodeLevel;
+    if(fontLevel > 5) {
+      fontLevel = 5;
+    }
+    newTitleNode.innerHTML = `<span class="link link-${fontLevel}">${titleNode.NodeText}</span>`;
     parentNode.appendChild(newTitleNode);
-    if(titleNode.Children.length > 0) {
+    if (titleNode.Children.length > 0) {
       const ul = document.createElement('ul');
       ul.id = `${newTitleNode.id}_ul`;
       newTitleNode.appendChild(ul);
@@ -79,27 +83,24 @@ function RenderTitles(nodeId: string, nodeList: TitleNode[]) {
   }
 }
 
-function UpdateRanges(titleList: TitleNode[]){
+function UpdateRanges(titleList: TitleNode[]) {
   const workTitles = titleList.concat([titleList.at(-1)!]);
   const lastIndex = workTitles.length - 1;
   workTitles.forEach((item, index) => {
-    if(index !== 0)  {
+    if (index !== 0) {
       const prevTitle = workTitles[index - 1];
       const currentTitle = workTitles[index];
       const range = document.createRange();
       range.setStart(document.querySelector(`[data-title-id="${prevTitle.NodeId}"]`)!, 0);
       const endElement = document.querySelector(`[data-title-id="${currentTitle.NodeId}"]`)!;
-      if(lastIndex === index) {
-       console.log(lastIndex) ;
-      }
       range.setEnd(endElement, lastIndex === index ? 1 : 0);
       range.cloneContents().querySelectorAll('*').forEach((element, index) => {
         const uid = element.getAttribute('data-uid');
         const subElement = document.querySelector(`[data-uid="${uid}"`);
-        if(subElement) {
-            const targetNodeId = subElement.getAttribute('data-title-id');
-            subElement.setAttribute('data-scroll-id', targetNodeId === currentTitle.NodeId ? currentTitle.NodeId : prevTitle.NodeId);
-            subElement.removeAttribute('data-uid');
+        if (subElement) {
+          const targetNodeId = subElement.getAttribute('data-title-id');
+          subElement.setAttribute('data-scroll-id', targetNodeId === currentTitle.NodeId ? currentTitle.NodeId : prevTitle.NodeId);
+          //subElement.removeAttribute('data-uid');
         }
       });
     }
@@ -110,49 +111,54 @@ const titleList = CreateTitleList();
 RenderTitles('titles', titleList);
 UpdateRanges(titleList);
 
-const observer = new IntersectionObserver(TitlesIntersected,{root: document.getElementById('container-top')});
+let isLoading = true;
+const observer = new IntersectionObserver(TitlesIntersected, { root: document.getElementById('container-top') });
 const hs = document.querySelectorAll('.container-top [data-scroll-id]');
 hs.forEach(h => observer.observe(h));
 
-const mapScrollId = new Map<string, number>();
-function TitlesIntersected(entries: IntersectionObserverEntry[], observer: IntersectionObserver){
-  entries.forEach(({boundingClientRect,
+const mapScrollId = new Map<string, Map<string, boolean>>();
+
+function TitlesIntersected(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
+  entries.forEach(({ boundingClientRect,
     intersectionRatio,
     intersectionRect,
     isIntersecting,
     rootBounds,
     target,
-    time}) => {
-      const scrollId = target.getAttribute('data-scroll-id')!;
-      const titleLi = document.getElementById(scrollId)!;
-      const titleSpan = titleLi.querySelector('a:first-child')!;
-      let value = 0;
-      if(mapScrollId.has(scrollId)) {
-        value = mapScrollId.get(scrollId)!;
-      }
+    time }) => {
+    const scrollId = target.getAttribute('data-scroll-id')!;
+    const titleLi = document.getElementById(scrollId)! as HTMLLIElement
+    const titleSpan = titleLi.querySelector('.link:first-child')! as HTMLSpanElement;
+    const pid = target.getAttribute('data-uid')!;
+    let value: Map<string, boolean>;
 
-      if(isIntersecting) {
-        value++;
-      } else{
-        if(value > 0) {
-          value--;
-        }
-      }
-      if(value === 0) {
-        titleSpan.classList.remove('title-selected');
-      } else {
-        if(!titleSpan.classList.contains('title-selected')){
-          titleSpan.classList.add('title-selected');
-        }
-      }
+    if (mapScrollId.has(scrollId)) {
+      value = mapScrollId.get(scrollId)!;
+    } else {
+      value = new Map<string, boolean>();
       mapScrollId.set(scrollId, value);
+    }
+    value.set(pid, isIntersecting);
+    let isRegionVisible = false;
+    value.forEach(v => isRegionVisible ||= v);
+
+    if (isRegionVisible) {
+      if (!titleSpan.classList.contains('title-selected')) {
+        titleSpan.classList.add('title-selected');
+      }
+    } else {
+      titleSpan.classList.remove('title-selected');
+    }
   });
 }
 
-function Log(message: string){
-  const logContainer = document.getElementById('log')!;
-  const logMessage = document.createElement('p')!;
-  logMessage.className = "log-message";
-  logMessage.textContent = message;
-  logContainer.appendChild(logMessage);
-}
+document.querySelector('.side')!.addEventListener('click', (e) =>{
+  if(e.target instanceof HTMLElement) {
+    const li = e.target.closest('li');
+    if(li) {
+      const container = document.querySelector('.container-top')!;
+      const h = container.querySelector(`[data-title-id="${li!.id}"]`)!;
+      h.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+});
